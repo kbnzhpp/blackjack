3f2fa6e1b0e5a173cbd10e7c5736f06c47d2990c
kbnzhpp <pakseevf@mail.ru>
2024-11-13 03:28:12 +0700

    blackjack v1.0.2

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-blackjack game on python console V1.0.1
+blackjack game on python console V1.0.2
 
+v1.0.2
+- now youre able to print moves in lower case
+- interface update
+- files unification in main.py
+- split bid count fix (especially with double)
+
+v1.0.1
 - checker of moves rebuild
 - moves function fix
 - fix with zero bid
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random_picker as r_p
-from cards import Card
+import random as rd
 
 """
     !!! 28.10.24 - 30.10.24 --- TRY CODE THIS STATEMENTS !!!
@@ -13,6 +12,12 @@ from cards import Card
 #31.10 finished logic of split(close to finish, had bugs(so tired today, need lil break)) tomorrow just need to debug code, think this would help to find mitekes in code structure
 #01.11 did start_game's functions global, finished working split, kinda working hit and stand, on this time bugs werent found by me, current_hand added, returned to split functions of showing dealer and players cards. tomorrow need to write double function, insurance function, if possible start to write count bids 
 
+#Card class
+class Card:
+    def __init__(self, number: str, suit: str):
+        self.suit = suit
+        self.number = number
+        self.card = [self.number, self.suit]
 
 #Start screen
 def start_game():
@@ -21,19 +26,19 @@ def start_game():
     global current_bid
     global dealer_card2_disclosed
     global current_hand
-    player_card1 = r_p.pick_random()
-    player_card2 = r_p.pick_random()
+    player_card1 = Card('2', '')
+    player_card2 = Card('2', '')
     player_hand = [player_card1, player_card2]
-    dealer_card1 = r_p.pick_random()
+    dealer_card1 = pick_random()
     dealer_card2 = Card('Hidden card', '')
-    dealer_card2_disclosed = r_p.pick_random()
+    dealer_card2_disclosed = pick_random()
     if dealer_card1.number == 'A':
         dealer_hand = [dealer_card1, dealer_card2_disclosed]
     else:
         dealer_hand = [dealer_card1, dealer_card2]
     current_hand = player_hand
     
-    print(f'You started new game\nYour balance: {balance_player}.')
+    print(f'Your balance: {balance_player}.')
     bid = do_bid()
     while bid > balance_player:
         print('Make smaller bid, your current bid bigger than your balance')
@@ -45,8 +50,8 @@ def start_game():
 
     check_available_moves(current_hand)
     moves()
-    print(count_values(current_hand))
-    print(dealer_moves())
+    print(f'Your final score: {count_values(current_hand)}')
+    print(f'Dealer final score: {dealer_moves()}')
 
     player_value = count_values(current_hand)
     dealer_value = count_values(dealer_hand)
@@ -61,6 +66,7 @@ def start_game():
 
     print_result(current_bid, result)
     
+#Available moves check
 def check_available_moves(hand):
     for key in available_moves_hashmap.keys():
         match key:
@@ -92,39 +98,46 @@ def moves():
     global current_hand
     global current_bid
     while True and count_values(current_hand) <= 21:
-        yes_or_no = input("What's your next move? Print SUR if you want to surrend, DB if double, SPL if split, HIT if +card, STAND if you dont need new card\n")
-        match yes_or_no:
-            case "SPL":
+        yes_or_no = input("What's your next move? SUR(to surrender), HIT(to hit), STAND(to stand), DB(to double), SPL(to split)\n")
+        match yes_or_no.lower():
+            case "spl":
                 if available_moves_hashmap['SPL'] == True:
-                    current_bid *= 2
                     available_moves_hashmap['SPL'] = False
-                    split1, split2 = [player_hand[0], r_p.pick_random()], [player_hand[-1], r_p.pick_random()]
+                    split1, split2 = [player_hand[0], pick_random()], [player_hand[-1], pick_random()]
                     player_hand1 = split1
                     player_hand2 = split2
                     current_hand = player_hand1
+                    bid1 = bid2 = current_bid
                     cards_show(current_hand)
                     moves()
+                    bid1 = current_bid
+                    print(f'Your final score: {count_values(current_hand)}')
                     current_hand = player_hand2
+                    current_bid = bid2
                     check_available_moves(current_hand)
                     available_moves_hashmap['SPL'] = False
                     cards_show(current_hand)
                     moves()
+                    bid2 = current_bid
+                    print(bid1, bid2)
+                    current_bid = bid1 + bid2
+                    print(current_bid)
                     break
                 else: print('Cannot split there')
-            case 'DB':
+            case 'db':
                 if available_moves_hashmap['DB'] == True:
                     current_bid *= 2
-                    current_hand.append(r_p.pick_random())
+                    current_hand.append(pick_random())
                     cards_show(current_hand)
                     break
                 else:
                     print('Double can be made only when you have 2 cards in hand')
-            case 'HIT':
-                current_hand.append(r_p.pick_random())
+            case 'hit':
+                current_hand.append(pick_random())
                 cards_show(current_hand)
-            case "STAND":
+            case "stand":
                 break
-            case 'SUR':
+            case 'sur':
                 global balance_player
                 current_bid /= 2
                 balance_player -= current_bid
@@ -148,6 +161,7 @@ def cards_show_dealer():
         print(f"| {' '.join(i.card)} |")
     print('\n')
 
+#Count hand
 def count_values(hand):
     total_value = 0
     num_aces = 0
@@ -164,17 +178,19 @@ def count_values(hand):
 
     return total_value
 
+#Dealer's moves after player's
 def dealer_moves():
     while True:
         dealer_hand[1] = dealer_card2_disclosed
         x = count_values(dealer_hand)
-        cards_show_dealer()
+        
         if x > 17:
             break
-        dealer_hand.append(r_p.pick_random())
+        dealer_hand.append(pick_random())
+    cards_show_dealer()
     return x
 
-
+#Making bid and chekicng validation of it
 def do_bid():
     while True:
         try:
@@ -186,6 +202,7 @@ def do_bid():
         except AssertionError:
             print('Bid cannot be lower or equal to zero.')
 
+#Show final result function
 def print_result(bid, result):
     global balance_player
     if result == "win":
@@ -198,8 +215,17 @@ def print_result(bid, result):
         print(f"It's a draw! Balance remains the same: {balance_player}")
     else:
         print("Invalid result")
-    
 
+#Random card picker
+def pick_random():
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits =  ['♦', '♣', '♥', '♠']
+
+    number, suit = rd.choice(ranks), rd.choice(suits)
+    card = Card(number, suit)
+    return card
+
+    
 numbers = {
         '2' : 2,
         '3' : 3,
@@ -226,8 +252,11 @@ available_moves_hashmap = {
 
 balance_player = 1000
 
+#Game would be infitity, while player have money or player himself wouldnt
+print('------------------  | BLACKJACK |   ------------------')
 while balance_player > 0 :
     start_game()
-    inp = input('Wanna play another? Y or N\n')
-    if inp != 'Y':
+    inp = input('Wanna play another? Y or N\n').lower()
+    if inp != 'y':
+        print('Thanks for playing!')
         break
